a0e0de5f82d64e49e147716d7a0948061f3bc2e3
Bastian Schwarz <bastian@codename-php.de>
2017-10-07 02:13:09 +0200

    Updated to phpunit6

--- src/test/php/de/codenamephp/platform/core/file/property/reader/StreamTest.php
+++ src/test/php/de/codenamephp/platform/core/file/property/reader/StreamTest.php
@@ -1,12 +1,12 @@
 <?php
-/*
- * Copyright 2014 Bastian Schwarz <bastian@codename-php.de>.
+/**
+ * Copyright 2017 CodenamePHP <mail@codename-php.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,15 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * @namespace
- */
+
 namespace de\codenamephp\platform\core\file\property\reader;
 
-use \de\codenamephp\platform\core\file\property\Entry;
-use \de\codenamephp\platform\core\TestCase;
-use \org\bovigo\vfs\vfsStream;
-use \SplFileObject;
+use de\codenamephp\platform\core\file\property\Entry;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+use SplFileObject;
 
 /**
  *
@@ -44,21 +42,21 @@ class StreamTest extends TestCase {
     vfsStream::setup();
   }
 
-  public function testread_CanReturnFile() {
-    vfsStream::create(array('testfile.properties' => ''));
-
-    self::assertInstanceOf('\de\codenamephp\platform\core\file\property\Entries', $this->sut->read(new SplFileObject(vfsStream::url('root/testfile.properties'))));
-  }
-
   public function testread_CanReadPropertiesFromFile() {
-    vfsStream::create(array('testfile.properties' => implode(PHP_EOL, array('prop1=value1', 'prop2=value2', 'prop3=value3'))));
+    vfsStream::create(['testfile.properties' => implode(PHP_EOL, ['prop1=value1', 'prop2=value2', 'prop3=value3'])]);
 
     $file = $this->sut->read(new SplFileObject(vfsStream::url('root/testfile.properties')));
 
-    self::assertEquals(array(
-        'prop1' => new Entry('prop1', 'value1'),
-        'prop2' => new Entry('prop2', 'value2'),
-        'prop3' => new Entry('prop3', 'value3'),
-            ), $file->getEntries());
+    self::assertEquals([
+      'prop1' => new Entry('prop1', 'value1'),
+      'prop2' => new Entry('prop2', 'value2'),
+      'prop3' => new Entry('prop3', 'value3'),
+    ], $file->getEntries());
+  }
+
+  public function testread_CanReturnFile() {
+    vfsStream::create(['testfile.properties' => '']);
+
+    self::assertInstanceOf('\de\codenamephp\platform\core\file\property\Entries', $this->sut->read(new SplFileObject(vfsStream::url('root/testfile.properties'))));
   }
 }
--- src/test/php/de/codenamephp/platform/core/file/property/writer/StreamTest.php
+++ src/test/php/de/codenamephp/platform/core/file/property/writer/StreamTest.php
@@ -1,12 +1,12 @@
 <?php
-/*
- * Copyright 2014 Bastian Schwarz <bastian@codename-php.de>.
+/**
+ * Copyright 2017 CodenamePHP <mail@codename-php.de>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,21 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * @namespace
- */
+
 namespace de\codenamephp\platform\core\file\property\writer;
 
 use de\codenamephp\platform\core\file\property\Entries;
 use de\codenamephp\platform\core\file\property\Entry;
-use de\codenamephp\platform\core\TestCase;
 use org\bovigo\vfs\vfsStream;
 
 /**
  *
  * @author Bastian Schwarz <bastian@codename-php.de>
  */
-class StreamTest extends TestCase {
+class StreamTest extends \PHPUnit\Framework\TestCase {
 
   /**
    *
@@ -53,9 +50,9 @@ class StreamTest extends TestCase {
 
     $propertyFile = new Entries();
     $propertyFile
-            ->addEntry(new Entry('prop1', 'value1'))
-            ->addEntry(new Entry('prop2', 'value2'))
-            ->addEntry(new Entry('prop3', 'value3'));
+      ->addEntry(new Entry('prop1', 'value1'))
+      ->addEntry(new Entry('prop2', 'value2'))
+      ->addEntry(new Entry('prop3', 'value3'));
 
     $this->sut->write($file, $propertyFile);
   }
